eaee2dff30f4cb2b9173b4948f1019a7ed7875bd
uncenter <me@uncenter.org>
2023-04-14 11:47:54 -0400

    fix: add vercel adapter to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
+import vercel from "solid-start-vercel";
 
 export default defineConfig({
     server: {
         port: 3000,
     },
-    plugins: [solid()],
+    plugins: [solid({ adapter: vercel() })],
 });
