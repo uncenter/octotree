403743e313388fe90774b96acf84dfd1ec19e742
uncenter <me@uncenter.org>
2023-04-14 15:24:25 -0400

    build: use latest solid-start vercel adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
-// @ts-expect-error
 import vercel from "solid-start-vercel";
 
 export default defineConfig({
     server: {
         port: 3000,
     },
-    plugins: [solid({ adapter: vercel() })],
+    plugins: [solid({ adapter: vercel({}) })],
 });
