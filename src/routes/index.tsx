d84a5012a06ce290a6c6b2400bd83a7cc506459f
uncenter <me@uncenter.org>
2023-05-13 22:27:41 -0400

    feat: new copy button, button color changes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -156,7 +156,7 @@ export default function App() {
                 </Input.Root>
                 <Button.Root
                     type="submit"
-                    class="btn outline green mt-6 justify-center"
+                    class="btn pink outline blue mt-6 justify-center"
                 >
                     Get Tree
                 </Button.Root>
@@ -196,200 +196,147 @@ export default function App() {
                         </Show>
                     </Show>
                 </pre>
+            </div>
+            <div class="flex flex-row justify-center">
                 <Button.Root
-                    aria-label="Copy to clipboard"
-                    class="hover:scale-110 absolute right-4 top-4 transition-transform duration-200"
-                    onClick={(e) => {
-                        const button = e.currentTarget;
-                        if (document.querySelector("pre")?.textContent !== "") {
-                            navigator.clipboard.writeText(
-                                tree.result as string
-                            );
-                        }
-                        button.innerHTML = `<svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        stroke="#2bbd50"
-                        fill="none"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        width="1em"
-                        height="1em"
-                        viewBox="0 0 24 24"
-                    >
-                        <polyline points="20 6 9 17 4 12"></polyline>
-                    </svg>`;
-                        setTimeout(() => {
-                            button.innerHTML = `<svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            stroke="#a1a1aa"
-                            fill="none"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            width="1em"
-                            height="1em"
-                            viewBox="0 0 24 24"
-                        >
-                            <rect
-                                x="9"
-                                y="9"
-                                width="13"
-                                height="13"
-                                rx="2"
-                                ry="2"
-                            ></rect>
-                            <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
-                        </svg>`;
-                        }, 2000);
+                    aria-label="Copy tree to clipboard"
+                    class="btn outline blue mt-6 justify-center"
+                    disabled={tree.pending || tree.error || !tree.result}
+                    onClick={() => {
+                        navigator.clipboard.writeText(
+                            buildTree(tree.result, state)
+                        );
                     }}
                 >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        stroke="#a1a1aa"
-                        fill="none"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        width="1em"
-                        height="1em"
-                        viewBox="0 0 24 24"
-                    >
-                        <rect
-                            x="9"
-                            y="9"
-                            width="13"
-                            height="13"
-                            rx="2"
-                            ry="2"
-                        ></rect>
-                        <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
-                    </svg>
+                    Copy
                 </Button.Root>
-            </div>
-            <AlertDialog.Root>
-                <AlertDialog.Trigger
-                    class="btn outline purple mt-6 justify-center"
-                    disabled={tree.pending || tree.error || !tree.result}
-                >
-                    Export as JSON
-                </AlertDialog.Trigger>
-                <AlertDialog.Portal>
-                    <AlertDialog.Overlay class="alert-overlay" />
-                    <div class="alert-positioner">
-                        <AlertDialog.Content class="rounded-md bg-bg p-6">
-                            <div class="flex items-baseline justify-between mb-3">
-                                <AlertDialog.Title class="text-xl font-medium text-fg">
-                                    Export as JSON
-                                </AlertDialog.Title>
-                                <AlertDialog.CloseButton class="h-6 w-6 text-fg">
-                                    <OcX2 />
-                                </AlertDialog.CloseButton>
-                            </div>
-                            <AlertDialog.Description class="text-fg text-sm">
-                                <RadioGroup.Root
-                                    class="flex flex-col gap-2"
-                                    value={exportOption()}
-                                    onChange={setExportOption}
-                                >
-                                    <RadioGroup.Label class="text-sm font-medium text-gray-900 dark:text-gray-300">
-                                        Export as...
-                                    </RadioGroup.Label>
-                                    <div class="flex gap-4">
-                                        <For
-                                            each={[
-                                                "Array of paths",
-                                                "Nested tree object",
-                                            ]}
-                                        >
-                                            {(option) => (
-                                                <RadioGroup.Item
-                                                    value={option}
-                                                    class="flex items-center"
-                                                >
-                                                    <RadioGroup.ItemInput class="radio__input" />
-                                                    <RadioGroup.ItemControl class="flex items-center justify-center h-5 w-5 border border-zinc-300 bg-zinc-200 rounded-[10px] border-solid data-[checked]:bg-[hsl(200_98%_39%)] data-[checked]:border-[hsl(200_98%_39%)]">
-                                                        <RadioGroup.ItemIndicator class="h-2.5 w-2.5 bg-[white] rounded-[5px]" />
-                                                    </RadioGroup.ItemControl>
-                                                    <RadioGroup.ItemLabel class="text-zinc-900 text-sm select-none ml-1.5">
-                                                        {option}
-                                                    </RadioGroup.ItemLabel>
-                                                </RadioGroup.Item>
-                                            )}
-                                        </For>
-                                    </div>
-                                </RadioGroup.Root>
-                                <div class="flex flex-row justify-center">
-                                    <Button.Root
-                                        class="btn outline purple mt-6 justify-center"
-                                        onClick={() => {
-                                            if (!tree.result || !exportOption())
-                                                return;
-                                            if (
-                                                exportOption() ===
-                                                "Array of paths"
-                                            ) {
-                                                downloadJson(
-                                                    JSON.stringify(
-                                                        flattenPaths(
-                                                            tree.result
-                                                        ),
-                                                        null,
-                                                        4
-                                                    ),
-                                                    "tree.json"
-                                                );
-                                            } else {
-                                                downloadJson(
-                                                    JSON.stringify(
-                                                        tree.result,
-                                                        null,
-                                                        4
-                                                    ),
-                                                    "tree.json"
-                                                );
-                                            }
-                                        }}
-                                    >
-                                        Download
-                                    </Button.Root>
-                                    <Button.Root
-                                        class="btn outline purple mt-6 justify-center"
-                                        onClick={() => {
-                                            if (!tree.result || !exportOption())
-                                                return;
-                                            if (
-                                                exportOption() ===
-                                                "Array of paths"
-                                            ) {
-                                                navigator.clipboard.writeText(
-                                                    JSON.stringify(
-                                                        flattenPaths(
-                                                            tree.result
-                                                        ),
-                                                        null,
-                                                        4
-                                                    )
-                                                );
-                                            } else {
-                                                navigator.clipboard.writeText(
-                                                    JSON.stringify(
-                                                        tree.result,
-                                                        null,
-                                                        4
-                                                    )
-                                                );
-                                            }
-                                        }}
-                                    >
-                                        Copy
-                                    </Button.Root>
+                <AlertDialog.Root>
+                    <AlertDialog.Trigger
+                        class="btn outline yellow mt-6 justify-center"
+                        disabled={tree.pending || tree.error || !tree.result}
+                    >
+                        Export JSON
+                    </AlertDialog.Trigger>
+                    <AlertDialog.Portal>
+                        <AlertDialog.Overlay class="alert-overlay" />
+                        <div class="alert-positioner">
+                            <AlertDialog.Content class="rounded-md bg-bg p-6">
+                                <div class="flex items-baseline justify-between mb-3">
+                                    <AlertDialog.Title class="text-xl font-medium text-fg">
+                                        Export JSON as...
+                                    </AlertDialog.Title>
+                                    <AlertDialog.CloseButton class="h-6 w-6 text-fg">
+                                        <OcX2 />
+                                    </AlertDialog.CloseButton>
                                 </div>
-                            </AlertDialog.Description>
-                        </AlertDialog.Content>
-                    </div>
-                </AlertDialog.Portal>
-            </AlertDialog.Root>
+                                <AlertDialog.Description class="text-fg text-sm">
+                                    <RadioGroup.Root
+                                        class="flex flex-col gap-2 mt-6 mb-2 mx-4"
+                                        value={exportOption()}
+                                        onChange={setExportOption}
+                                    >
+                                        <div class="flex gap-4">
+                                            <For
+                                                each={[
+                                                    "Array of paths",
+                                                    "Nested tree object",
+                                                ]}
+                                            >
+                                                {(option) => (
+                                                    <RadioGroup.Item
+                                                        value={option}
+                                                        class="flex items-center"
+                                                    >
+                                                        <RadioGroup.ItemInput class="radio__input" />
+                                                        <RadioGroup.ItemControl class="flex items-center justify-center h-5 w-5 border border-zinc-300 bg-zinc-200 rounded-[10px] border-solid data-[checked]:bg-[hsl(200_98%_39%)] data-[checked]:border-[hsl(200_98%_39%)]">
+                                                            <RadioGroup.ItemIndicator class="h-2.5 w-2.5 bg-[white] rounded-[5px]" />
+                                                        </RadioGroup.ItemControl>
+                                                        <RadioGroup.ItemLabel class="text-zinc-900 text-sm select-none ml-1.5">
+                                                            {option}
+                                                        </RadioGroup.ItemLabel>
+                                                    </RadioGroup.Item>
+                                                )}
+                                            </For>
+                                        </div>
+                                    </RadioGroup.Root>
+                                    <div class="flex flex-row justify-center">
+                                        <Button.Root
+                                            class="btn outline purple mt-6 justify-center"
+                                            onClick={() => {
+                                                if (
+                                                    !tree.result ||
+                                                    !exportOption()
+                                                )
+                                                    return;
+                                                if (
+                                                    exportOption() ===
+                                                    "Array of paths"
+                                                ) {
+                                                    downloadJson(
+                                                        JSON.stringify(
+                                                            flattenPaths(
+                                                                tree.result
+                                                            ),
+                                                            null,
+                                                            4
+                                                        ),
+                                                        "tree.json"
+                                                    );
+                                                } else {
+                                                    downloadJson(
+                                                        JSON.stringify(
+                                                            tree.result,
+                                                            null,
+                                                            4
+                                                        ),
+                                                        "tree.json"
+                                                    );
+                                                }
+                                            }}
+                                        >
+                                            Download
+                                        </Button.Root>
+                                        <Button.Root
+                                            class="btn outline purple mt-6 justify-center"
+                                            onClick={() => {
+                                                if (
+                                                    !tree.result ||
+                                                    !exportOption()
+                                                )
+                                                    return;
+                                                if (
+                                                    exportOption() ===
+                                                    "Array of paths"
+                                                ) {
+                                                    navigator.clipboard.writeText(
+                                                        JSON.stringify(
+                                                            flattenPaths(
+                                                                tree.result
+                                                            ),
+                                                            null,
+                                                            4
+                                                        )
+                                                    );
+                                                } else {
+                                                    navigator.clipboard.writeText(
+                                                        JSON.stringify(
+                                                            tree.result,
+                                                            null,
+                                                            4
+                                                        )
+                                                    );
+                                                }
+                                            }}
+                                        >
+                                            Copy
+                                        </Button.Root>
+                                    </div>
+                                </AlertDialog.Description>
+                            </AlertDialog.Content>
+                        </div>
+                    </AlertDialog.Portal>
+                </AlertDialog.Root>
+            </div>
         </Page>
     );
 }
