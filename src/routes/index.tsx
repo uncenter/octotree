dd085195ef380f0785f223cec75b9a830a61cde5
uncenter <me@uncenter.org>
2023-06-07 11:17:20 -0400

    refactor: darker error color

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -189,7 +189,7 @@ export default function App() {
                 <pre class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 h-[16em] overflow-scroll">
                     <Show when={!tree.pending} fallback={"Loading..."}>
                         <Show when={tree.error}>
-                            <p class="text-red-500 dark:text-red-400 text-center mt-4 font-bold">
+                            <p class="text-red-600 dark:text-red-500 text-center mt-4 font-bold">
                                 {tree.error.message}
                             </p>
                         </Show>
