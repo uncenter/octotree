d97475e4eaf11646277a966d1f476497cd5276c9
uncenter <47499684+uncenter@users.noreply.github.com>
2023-08-26 16:01:29 -0400

    fix: update analytics url

--- src/root.tsx
+++ src/root.tsx
@@ -1,72 +1,64 @@
 // @refresh reload
-import { Suspense, createSignal } from "solid-js";
+import { Suspense, createSignal } from 'solid-js';
 import {
-    Body,
-    ErrorBoundary,
-    FileRoutes,
-    Head,
-    Html,
-    Link,
-    Meta,
-    Routes,
-    Scripts,
-    Title,
-} from "solid-start";
-import "./root.css";
-import { isServer } from "solid-js/web";
+	Body,
+	ErrorBoundary,
+	FileRoutes,
+	Head,
+	Html,
+	Link,
+	Meta,
+	Routes,
+	Scripts,
+	Title,
+} from 'solid-start';
+import './root.css';
+import { isServer } from 'solid-js/web';
 
 export default function Root() {
-    const [isDark, setIsDark] = createSignal(
-        isServer
-            ? false
-            : window.matchMedia("(prefers-color-scheme: dark)").matches
-    );
-    if (!isServer) {
-        window
-            .matchMedia("(prefers-color-scheme: dark)")
-            .addEventListener("change", (e) => {
-                setIsDark(e.matches);
-            });
-    }
+	const [isDark, setIsDark] = createSignal(
+		isServer
+			? false
+			: window.matchMedia('(prefers-color-scheme: dark)').matches,
+	);
+	if (!isServer) {
+		window
+			.matchMedia('(prefers-color-scheme: dark)')
+			.addEventListener('change', (e) => {
+				setIsDark(e.matches);
+			});
+	}
 
-    return (
-        <Html lang="en">
-            <Head>
-                <Title>Octotree</Title>
-                <Meta
-                    name="description"
-                    content="Octotree - turn Github repositories into ASCII trees."
-                />
-                <Meta name="author" content="uncenter" />
-                <Meta charset="utf-8" />
-                <Meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
-                <Meta name="color-scheme" content="light dark" />
-                <Meta
-                    name="theme-color"
-                    content={isDark() ? "#18181b" : "#ffffff"}
-                />
-                <Link rel="icon" type="image/x-icon" href="favicon.ico" />
-                <script
-                    async
-                    defer
-                    data-website-id="5fdeff0b-1326-478c-b06a-e03694f60553"
-                    data-domains="tree.uncenter.org"
-                    src="https://stats.uncenter.org/beepboop.js"
-                ></script>
-            </Head>
-            <Body>
-                <Suspense>
-                    <ErrorBoundary>
-                        <Routes>
-                            <FileRoutes />
-                        </Routes>
-                    </ErrorBoundary>
-                </Suspense>
-                <Scripts />
-            </Body>
-        </Html>
-    );
+	return (
+		<Html lang="en">
+			<Head>
+				<Title>Octotree</Title>
+				<Meta
+					name="description"
+					content="Octotree - turn Github repositories into ASCII trees."
+				/>
+				<Meta name="author" content="uncenter" />
+				<Meta charset="utf-8" />
+				<Meta name="viewport" content="width=device-width, initial-scale=1" />
+				<Meta name="color-scheme" content="light dark" />
+				<Meta name="theme-color" content={isDark() ? '#18181b' : '#ffffff'} />
+				<Link rel="shortcut icon" href="favicon.ico" />
+				<script
+					async
+					data-website-id="5fdeff0b-1326-478c-b06a-e03694f60553"
+					src="https://analytik.uncenter.dev/script.js"
+				></script>
+			</Head>
+			<Body>
+				<Suspense>
+					<ErrorBoundary>
+						<Routes>
+							<FileRoutes />
+						</Routes>
+					</ErrorBoundary>
+				</Suspense>
+				<Scripts />
+			</Body>
+		</Html>
+	);
 }
